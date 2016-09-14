28eff50f27ad9a76d202d9215de555922973d299
gerrit thomson <gerrit.thomson@gmail.com>
2016-09-14 10:51:41 +1000

    First Commit

---
+++ cloudant_config.js
@@ -0,0 +1,5 @@
+/**
+ * Created by ADMIN on 2/09/2016.
+ */
+var cloudant_config = {url: 'gerrit.cloudant.com/kittens', api_key: 'hadarremandisdourraboake', api_password: '8486802219902fb4df32beba9837f10e67c2398d'};
+
